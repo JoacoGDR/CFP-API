7c43823dc19813664fde22541d039fb48ed57a4b
Joaco <jogarcia@itba.edu.ar>
2024-10-20 00:08:55 -0300

    feat: add incidental to catches table

--- db/migrate/20241019173710_create_catches.rb
+++ db/migrate/20241019173710_create_catches.rb
@@ -1,6 +1,7 @@
 class CreateCatches < ActiveRecord::Migration[7.2]
   def change
     create_table :catches do |t|
+      t.boolean :incidental, null: false, default: false
       t.references :vessel, foreign_key: true, null: false
       t.references :species, foreign_key: true, null: false
       t.float :weight, null: false
