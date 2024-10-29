0ad10cc091b656c01edd0c64396bf50eb20d6df0
Joaco <jogarcia@itba.edu.ar>
2024-10-28 23:27:56 -0300

    feat: initial auth modules

---
+++ app/helpers/credentials_helper.rb
@@ -0,0 +1,5 @@
+module CredentialsHelper
+  def self.fetch_secret(key, default = nil)
+    Rails.application.credentials.dig(Rails.env.to_sym, key) || default
+  end
+end
---
+++ app/models/consumer.rb
@@ -0,0 +1,35 @@
+# == Schema Information
+#
+# Table name: consumers
+#
+#  id         :bigint(8)        not null, primary key
+#  name       :string           not null
+#  api_key    :string           not null
+#  api_secret :string           not null
+#  last_nonce :datetime         not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class Consumer < ApplicationRecord
+  API_KEY_LENGTH = CredentialsHelper.fetch_secret(:consumer_key_length, default: 32)
+  API_SECRET_LENGTH = CredentialsHelper.fetch_secret(:consumer_secret_length, default: 64)
+
+  validates :name, presence: true
+  before_validation :generate_api_codes, on: :create
+
+  private
+
+  def generate_api_codes
+    self.api_key = generate_api_key
+    self.api_secret = generate_api_secret
+    self.last_nonce = Time.zone.now
+  end
+
+  def generate_api_key
+    Devise.friendly_token(API_KEY_LENGTH)
+  end
+
+  def generate_api_secret
+    Devise.friendly_token(API_SECRET_LENGTH)
+  end
+end
